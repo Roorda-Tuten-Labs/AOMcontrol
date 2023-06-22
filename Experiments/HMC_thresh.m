43cb897b64f761bb9235b0f51a101fbc152bb8b7
Gumphrey <35390641+Gumphrey@users.noreply.github.com>
2023-06-22 12:00:27 -0700

    mac edited script test for windows compatibility

---
+++ Experiments/HMC_thresh.m
@@ -0,0 +1,9 @@
+function HMC_thresh
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function is a wrapper to call HMC_thresh in the +HMC folder.
+%
+%  1/28/22 - LGJ
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+HMC.HMC_thresh();
+
+end
